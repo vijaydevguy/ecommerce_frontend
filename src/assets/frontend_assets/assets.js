2bd58668222a1c3bc444f4aea26ab1de74c26f82
vijaydevguy <vijayakumarjob01@gmail.com>
2025-12-21 21:30:57 +0530

    collection page & search component

--- src/assets/frontend_assets/assets.js
+++ src/assets/frontend_assets/assets.js
@@ -73,6 +73,7 @@ import contact_img from "./contact_img.png";
 import razorpay_logo from "./razorpay_logo.png";
 import stripe_logo from "./stripe_logo.png";
 import cross_icon from "./cross_icon.png";
+import no_results from "./no_result.svg";
 
 export const assets = {
   logo,
@@ -94,6 +95,7 @@ export const assets = {
   razorpay_logo,
   stripe_logo,
   cross_icon,
+  no_results,
 };
 
 export const products = [
